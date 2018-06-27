5ad571ac3228034f19fbeef379220e9d84a4f680
Pat <graczyk.pat@gmail.com>
2018-06-27 17:07:39 +0100

    first four done

--- exercise_a.rb
+++ exercise_a.rb
@@ -1,13 +1,18 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
-
 # Add "Edinburgh Waverley" to the end of the array
+stops[6] = "Edinburgh Waverley"
 # Add "Glasgow Queen St" to the start of the array
+stops.unshift("Glasgow Queen St")
 # Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+stops[3] = "Polmont"
 # Work out the index position of "Linlithgow"
+p stops.index("Linlithgow")
 # Remove "Livingston" from the array using its name
+
 # Delete "Cumbernauld" from the array by index
 # How many stops there are in the array?
 # How many ways can we return "Falkirk High" from the array?
 # Reverse the positions of the stops in the array
 # Print out all the stops using a for loop
+p stops
